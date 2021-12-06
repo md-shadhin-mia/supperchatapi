cbfb0ae4166fca1b85fe926710987fd5e37cec20
muhammadshadhin <mdshadhinkn@gmail.com>
2021-12-06 15:21:36 +0600

    mongo test working

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const express = require("express");
 const authRoute = require('./constroller/Auth');
 const superChatRoute = require('./constroller/superChat')
 
-const MONGOURL = process.env.MONGOURL || "mongodb://localhost:27017/message";
+const MONGOURL = process.env.MONGODB_URI || "mongodb://localhost:27017/message";
 const PORT = process.env.PORT || 5000;
 
 async function app()
 {
     const app = express()
-    // await mongoose.connect(MONGOURL);
-    // console.log("mongo db connected");
-    // app.use(express.json())
-    // app.use(express.urlencoded({extended:true}))
+    await mongoose.connect(MONGOURL);
+    console.log("mongo db connected");
+    app.use(express.json())
+    app.use(express.urlencoded({extended:true}))
 
-    // app.use("/auth", authRoute);
-    // app.use("/superchat", superChatRoute)
+    app.use("/auth", authRoute);
+    app.use("/superchat", superChatRoute)
     app.get("/", (req, res)=>{
         res.status(200).json({"message":"Welcome to Supper Chat API!"})
     })
