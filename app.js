21ee9446800b3c68fe27f1b09191dad435599a44
muhammadshadhin <mdshadhinkn@gmail.com>
2021-12-06 14:34:13 +0600

    disable mongo for test

--- app.js
+++ app.js
@@ -4,20 +4,19 @@ const authRoute = require('./constroller/Auth');
 const superChatRoute = require('./constroller/superChat')
 
 const MONGOURL = process.env.MONGOURL || "mongodb://localhost:27017/message";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 async function app()
 {
     const app = express()
-    await mongoose.connect(MONGOURL);
-    console.log("mongo db connected");
-    app.use(express.json())
-    app.use(express.urlencoded({extended:true}))
+    // await mongoose.connect(MONGOURL);
+    // console.log("mongo db connected");
+    // app.use(express.json())
+    // app.use(express.urlencoded({extended:true}))
 
-    app.use("/auth", authRoute);
-    app.use("/superchat", superChatRoute)
+    // app.use("/auth", authRoute);
+    // app.use("/superchat", superChatRoute)
     app.get("/", (req, res)=>{
-        console.log(req.headers.id,req.headers.username)
         res.status(200).json({"message":"Welcome to Supper Chat API!"})
     })
 
